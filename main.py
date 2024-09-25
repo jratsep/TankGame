22ba9f9863806a52e4fd0a2edecbfed96bccc28e
jratsep <j.ratsep@gmail.com>
2024-09-25 12:48:42 +0300

    Shoot logic added

--- main.py
+++ main.py
@@ -40,7 +40,15 @@ class Tank:
             print("Not present")
 
     def shoot(self, targetX, targetY):
-        return True
+        if self.direction == 'up' or self.direction == 'down':
+            if targetX == self.x:
+                if targetY > self.y and self.direction == 'up':
+                    return True
+                elif targetY < self.y and self.direction == 'down':
+                    return True
+                return False
+
+
 
 
 
